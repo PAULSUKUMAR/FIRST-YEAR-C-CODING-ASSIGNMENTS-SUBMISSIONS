983a99006dd75faa5eef919418094bc1caf480ab
PAULSUKUMAR <110917315+PAULSUKUMAR@users.noreply.github.com>
2022-08-16 21:33:17 +0530

    Add files via upload

---
+++ 16.08.2022 SECOND PROGRAM.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+     int a[10],n,i,j,count=0,prime[10],t=0;
+     
+     scanf("%d",&n);
+     
+     for(i=0;i<n;i++)
+     {
+          scanf("%d",&a[i]);
+     }
+     for(i=0;i<n;i++)
+     {
+          count=0;
+          for(j=2;j<a[i];j++)
+          {
+               if(a[i]%j==0)
+               {
+                    count=1;
+                    break;
+               }
+          }
+          if(count==0)
+          {
+               prime[t]=a[i];
+               t++;
+          }
+     }
+     
+     for(i=t-1;i>=0;i--)
+     {
+          printf(" %d ",prime[i]);
+     }
+     return 0;
+}1
